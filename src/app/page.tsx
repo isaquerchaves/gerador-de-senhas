18c51f6e2b6afcefc3318c35e15a259ee515250e
isaquerchaves <isaque.ramosch@gmail.com>
2023-10-28 15:47:07 -0300

    feat: adicionado input para escolher o tamanho da senha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { useState } from 'react';
 export default function Home() {
   const [password, setPassword] = useState("");
   const [copyText, setCopyText] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState(12)
 
-  function generate(){
+  function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12
     let newPassord = ''
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length)
       newPassord += characters[position]
     }
@@ -18,19 +18,30 @@ export default function Home() {
     setCopyText("Copiar")
   }
 
-  function copyToClipboard(){
+  function copyToClipboard() {
     window.navigator.clipboard.writeText(password)
     setCopyText("Copiado!")
   }
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-8">
-    <h1 className="text-5xl">Gerador de senhas</h1>
-    <div className="flex flex-row gap-8">
-      <button onClick={generate} className="rounded-xl p-2 border border-transparent bg-zinc-800 hover:border-purple-500">Gerar senha </button>
-      <button onClick={copyToClipboard} className="rounded-xl p-2 border border-transparent bg-zinc-800 hover:border-purple-500">{copyText}</button>
+      <h1 className="text-5xl">Gerador de senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input 
+          type="number" 
+          id="passwordSize" 
+          className=' ml-3 rounded-lg p-2 border border-transparent bg-zinc-800 hover:border-purple-500' 
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
+          />
+      </div>
+      <div className="flex flex-row gap-8">
+        <button onClick={generate} className="rounded-lg p-2 border border-transparent bg-zinc-800 hover:border-purple-500">Gerar senha de {passwordSize} caracteres</button>
+        <button onClick={copyToClipboard} className="rounded-lg p-2 border border-transparent bg-zinc-800 hover:border-purple-500">{copyText}</button>
+      </div>
+      <div>{password}</div>
     </div>
-    <div>{password}</div>
-  </div>
   )
 }
